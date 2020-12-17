ffdb8b211e502ba24b760fed47b06545642221ff
Hussam Heriz <hussam.heriz@gmail.com>
2020-12-17 10:29:15 +0200

    Finishing Login & Sign Up Controllers and Activities

--- app/src/main/java/hussamheriz/aug/todolistproject/LoginActivity.java
+++ app/src/main/java/hussamheriz/aug/todolistproject/LoginActivity.java
@@ -2,13 +2,45 @@ package hussamheriz.aug.todolistproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    TextView register;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailStr = email.getText().toString();
+                String passwordStr = password.getText().toString();
+                Toast.makeText(LoginActivity.this, emailStr+" "+passwordStr, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/hussamheriz/aug/todolistproject/SignUpActivity.java
+++ app/src/main/java/hussamheriz/aug/todolistproject/SignUpActivity.java
@@ -2,13 +2,53 @@ package hussamheriz.aug.todolistproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import hussamheriz.aug.todolistproject.Models.User;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText name, password, email;
+    Button register;
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
+
+        name        = findViewById(R.id.name);
+        password    = findViewById(R.id.password);
+        email       = findViewById(R.id.email);
+        register    = findViewById(R.id.register);
+        login       = findViewById(R.id.login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameStr = name.getText().toString();
+                String passwordStr = password.getText().toString();
+                String emailStr = email.getText().toString();
+
+                User user = new User(nameStr,emailStr, passwordStr);
+                Toast.makeText(SignUpActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignUpActivity.this, user.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
